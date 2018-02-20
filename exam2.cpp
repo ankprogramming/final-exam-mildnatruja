fec5c3110b569e25ff8c7abaa33ec93ae911b1d8
Unknown <mildnatruja@gmail.com>
2018-02-20 13:02:10 +0700

    2 เสร็จ

--- exam2.cpp
+++ exam2.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
-int main() {
+int main (){
+    int cmd;
+    string name;
+    string line;
+    ofstream writeFile;
+    ifstream readFile;
+    do{
+        cout << "MENU" <<endl;
+        cout << "1. Add student." << endl;
+        cout << "2. List student." << endl;
+        cout << "3. Search list student"<< endl;
+        cout << "4. Quit." << endl;
+        cout << "Please enter menu :";
+        cin>> cmd;
 
-        return 0;
+    if (cmd ==1) {
+        cout << "Enter std name :";
+        cin >> name;
+
+        writeFile.open("std.txt",fstream::app);
+        if (writeFile.is_open()){
+            writeFile << name << endl;
+            writeFile.close ();
+
+            } else { "unable to open file";
+
+            }
+    }else if (cmd==2){
+        readFile.open ("std.txt");
+        if (readFile.is_open ()){
+            while(getline(readFile, line)){
+                cout << line << endl;
+            }
+            readFile.close();
+        }
+    }else{
+        break;
+    }
+
+
+    }while(1);
+
+    return 0;
 }
